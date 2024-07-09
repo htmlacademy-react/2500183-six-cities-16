2a4d92d460e62fa031eeb73c736328020930789d
ivan <rineroriner@gmail.com>
2024-07-09 08:50:08 +0300

    создан файл констант

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-
-const Setting = {
-  OffersCount: 5
-} as const;
-
+import { Setting } from './const';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
