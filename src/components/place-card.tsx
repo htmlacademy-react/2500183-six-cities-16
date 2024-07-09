c104b01d965051b455ebd61491e523339c7a03aa
ivan <rineroriner@gmail.com>
2024-07-09 20:09:16 +0300

    доработка компонента place-card

--- src/components/place-card.tsx
+++ src/components/place-card.tsx
@@ -23,7 +23,7 @@ type PlaceCardProps = {
   };
 
 function PlaceCard({className = 'cities__card', place} : {className : string; place:PlaceCardProps}): JSX.Element {
-  const {price, type } = place;
+  const {price, type, title, previewImage,rating } = place;
   return (
     <article className={`${className} place-card`}>
       <div className="place-card__mark">
@@ -31,7 +31,7 @@ function PlaceCard({className = 'cities__card', place} : {className : string; pl
       </div>
       <div className="cities__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image" />
+          <img className="place-card__image" src={previewImage} width="260" height="200" alt="Place image" />
         </a>
       </div>
       <div className="place-card__info">
@@ -50,11 +50,11 @@ function PlaceCard({className = 'cities__card', place} : {className : string; pl
         <div className="place-card__rating rating">
           <div className="place-card__stars rating__stars">
             <span style={{width: '80%'}}></span>
-            <span className="visually-hidden">Rating</span>
+            <span className="visually-hidden">{rating}</span>
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+          <a href="#">Beautiful &amp; {title}</a>
         </h2>
         <p className="place-card__type">{type}</p>
       </div>
