413dfd1fbde6607392db1353fd57e711b19905db
ivan <rineroriner@gmail.com>
2024-07-12 19:02:17 +0300

    добавил линк компоненту header-logo

--- src/components/header/header-logo.tsx
+++ src/components/header/header-logo.tsx
@@ -1,9 +1,11 @@
+import {Link} from 'react-router-dom';
+
 function HeaderLogo() : JSX.Element {
   return (
     <div className="header__left">
-      <a className="header__logo-link" href="main.html">
+      <Link className="header__logo-link" to ='/'>
         <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
-      </a>
+      </Link>
     </div>
   );
 }
