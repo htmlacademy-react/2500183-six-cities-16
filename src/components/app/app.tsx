cedf7698d84715806ceba13eaf62f944556fd7ee
ivan <rineroriner@gmail.com>
2024-07-12 18:21:20 +0300

    прикоснулся к роутингу,для отладки страниц

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,17 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
 import Main from '../../pages/main/main';
+import Login from '../../pages/login/login';
+import Favorites from '../../pages/favorites/favorites';
+import Offer from '../../pages/offer/offer';
+
+
+enum AppRoute {
+  MainPage = '/',
+  LoginPage = '/login',
+  FavoritesPage = '/favorites',
+  OfferPage = '/offer'
+}
+
 
 type AppProps = {
   offersCount: number;
@@ -6,7 +19,28 @@ type AppProps = {
 
 function App({offersCount}: AppProps): JSX.Element {
   return (
-    <Main offersCount={offersCount} />
+    //<Main offersCount={offersCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.MainPage}
+          element={<Main offersCount={offersCount}/>}
+        />
+        <Route
+          path={AppRoute.LoginPage}
+          element={<Login/>}
+        />
+        <Route
+          path={AppRoute.FavoritesPage}
+          element={<Favorites/>}
+        />
+        <Route
+          path={AppRoute.OfferPage}
+          element={<Offer/>}
+        />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
