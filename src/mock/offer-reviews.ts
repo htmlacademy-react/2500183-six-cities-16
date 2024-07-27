4041819de16ae99df802704ec2239b4c91acc550
ivan <rineroriner@gmail.com>
2024-07-27 19:30:53 +0300

    созданы моки отзывов и их тип

---
+++ src/mock/offer-reviews.ts
@@ -0,0 +1,39 @@
+import { ReviewsT } from '../types/reviews/reviews';
+
+const offerReviews : ReviewsT[] = [
+  {
+    'id': 'b67ddfd5-b953-4a30-8c8d-bd083cd6b62a',
+    'date': '2019-05-08T14:13:56.569Z',
+    'user': {
+      'name': 'Oliver Conner',
+      'avatarUrl': 'img/avatar-max.jpg',
+      'isPro': false
+    },
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'rating': 4
+  },
+  {
+    'id': 'b67ddfd5-b953-4a30-8c8d-bd083cd6b62a',
+    'date': '2019-05-08T14:13:56.569Z',
+    'user': {
+      'name': 'Oliver Conner',
+      'avatarUrl': 'img/avatar-angelina.jpg',
+      'isPro': false
+    },
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'rating': 3
+  },
+  {
+    'id': 'b67ddfd5-b953-4a30-8c8d-bd083cd6b62a',
+    'date': '2019-05-08T14:13:56.569Z',
+    'user': {
+      'name': 'Oliver Conner',
+      'avatarUrl': 'img/avatar-angelina.jpg',
+      'isPro': false
+    },
+    'comment': 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.',
+    'rating': 2
+  }
+];
+
+export {offerReviews};
