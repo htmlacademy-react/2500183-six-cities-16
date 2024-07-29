a5b9d91fd3445295d53714e5a4bb9ca20137bff0
ivan <rineroriner@gmail.com>
2024-07-29 15:28:53 +0300

    выведено магическое число

--- src/components/review-form/review-form.tsx
+++ src/components/review-form/review-form.tsx
@@ -24,6 +24,8 @@ function ReviewForm(): JSX.Element {
     }
   };
 
+  const numberCharacters : number = 50;
+
   const reviewCheck = formData.review.length < REVIEW_LENGTH.MIN_REVIEW_LENGTH || formData.review.length > REVIEW_LENGTH.MAX_REVIEW_LENGTH || formData.rating === 0;
 
   return (
@@ -68,7 +70,7 @@ function ReviewForm(): JSX.Element {
       <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" onChange={reviewChangeHandler} value={formData.review}></textarea>
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
-        To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+        To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">{numberCharacters} characters</b>.
         </p>
         <button className="reviews__submit form__submit button" type="submit" disabled={reviewCheck}>Submit</button>
       </div>
