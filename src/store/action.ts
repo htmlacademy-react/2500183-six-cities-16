2f37f1ada1170c961daa8e5f55ef4d49136942d3
ivan <rineroriner@gmail.com>
2024-08-10 07:42:37 +0300

    переимениование действий

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,9 +1,8 @@
 import { createAction } from '@reduxjs/toolkit';
 import { SortBy } from '../const';
 
-const changeCity = createAction<{city: string}>('changeCity');
-const changeSortBy = createAction<{sortBy: SortBy}>('changeSortBy');
-
+const changeCity = createAction<{city: string}>('citiesList/changeCity');
+const changeSortBy = createAction<{sortBy: SortBy}>('placesSorting/changeSortBy');
 
 export {
   changeCity,
