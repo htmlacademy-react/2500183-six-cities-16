ba4825331a49bc0107a71bea7f93b676f179fd42
ivan <rineroriner@gmail.com>
2024-08-15 19:47:04 +0300

    подключил редьюсер полного  оффера к стору

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,7 @@ import { configureStore} from '@reduxjs/toolkit';
 import { createAPI } from '../services/api';
 import { mainReducer } from './main-reducer/main-reducer';
 import { userReducer } from './user-reducer/user-reducer';
+import { offerReducer } from './offer-reducer/offer-reducer';
 
 
 const api = createAPI();
@@ -9,7 +10,8 @@ const api = createAPI();
 const store = configureStore({
   reducer: {
     main: mainReducer,
-    user: userReducer
+    user: userReducer,
+    offerPage: offerReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
