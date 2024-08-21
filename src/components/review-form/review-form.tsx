84d4fcf6cc1e88c2081a94ce1a7b60a731aa7bad
ivan <rineroriner@gmail.com>
2024-08-21 12:33:11 +0300

    новый тест формы отзывов

--- src/components/review-form/review-form.tsx
+++ src/components/review-form/review-form.tsx
@@ -21,6 +21,7 @@ type ReviewFormProps = {
 
 function ReviewForm({ offerId }: ReviewFormProps): JSX.Element {
   const [submitButtonStatus, setSubmitButtonStatus] = useState(true);
+
   const [formData, setFormData] = useState<FormData>({
     rating: 0,
     review: ''
@@ -53,6 +54,7 @@ function ReviewForm({ offerId }: ReviewFormProps): JSX.Element {
   //const reviewStatus = useAppSelector(selectReviewStatus);
   const testStatus = useAppSelector(selectTestReviesStatus);
   //const isLoading = reviewStatus === RequestStatus.Loading;
+  const formDisableStatus = testStatus;
 
   //console.log(testStatus);
 
@@ -80,36 +82,36 @@ function ReviewForm({ offerId }: ReviewFormProps): JSX.Element {
     <form className="reviews__form form" action="#" method="post" onSubmit={handleSubmit}>
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating" onChange={raitingChangeHandler}>
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
-        <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
+        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" disabled={formDisableStatus}/>
+        <label htmlFor={(formDisableStatus) ? '' : '5-stars'} className="reviews__rating-label form__rating-label" title="perfect">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
-        <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
+        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" disabled={formDisableStatus}/>
+        <label htmlFor={(formDisableStatus) ? '' : '4-stars'} className="reviews__rating-label form__rating-label" title="good">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
-        <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
+        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" disabled={formDisableStatus}/>
+        <label htmlFor={(formDisableStatus) ? '' : '3-stars'} className="reviews__rating-label form__rating-label" title="not bad">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
-        <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
+        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"disabled={formDisableStatus} />
+        <label htmlFor={(formDisableStatus) ? '' : '2-stars'} className="reviews__rating-label form__rating-label" title="badly">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
-        <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
+        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"disabled={formDisableStatus} />
+        <label htmlFor={(formDisableStatus) ? '' : '1-stars'} className="reviews__rating-label form__rating-label" title="terribly">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
